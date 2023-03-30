57c2933a133739a56cba3c9cae94b5f21a2746ba
ArthurFernande <arthur.andrade@ccc.ufcg.edu.br>
2023-03-30 02:56:37 -0300

    atividade 02 correcao

--- src/pages/Video.js
+++ src/pages/Video.js
@@ -16,13 +16,13 @@ function Video() {
         }
     }
   return (
-    <div className='video'> 
+    <div className = 'video'> 
         <video 
-            className='video__player'
+            className = 'video__player'
             ref={videoRef}
             onClick={handleStart}
             loop
-            src={dog}
+            src="https://firebasestorage.googleapis.com/v0/b/video-2db61.appspot.com/o/X2Download.app-%EF%BF%BD%EF%BF%BD%EF%BF%BD%EF%BF%BD%EF%BF%BD%EF%BF%BD30%20Segundos%20status%EF%BF%BD%EF%BF%BD%EF%BF%BD%EF%BF%BD%EF%BF%BD%EF%BF%BD%23Shitpost%20%23Statusmemes%20%2330segundosparastatus%20%23Shitpostraiz%20%23memes(720p).mp4?alt=media&token=3aad9e8d-b49a-4ec0-bfa1-57849e1cca79"
             >
         </video>
     </div>
